71490dbbf6150f6ad9dc11527725411bafc4a082
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2023-04-23 15:05:38 -0300

    ajuste na BASE_URL para as rotas

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? 'https://ewertonvaz.github.io/trello-clone/' : process.env.BASE_URL,
   routes: [
     {
       path: '/',
