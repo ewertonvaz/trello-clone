6ebd29271ad24eb16038c44ded3fb61a75682640
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2023-04-23 15:23:26 -0300

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://ewertonvaz.github.io/trello-clone/' : '/'
+  // publicPath: process.env.NODE_ENV === 'production' ? 'https://ewertonvaz.github.io/trello-clone/' : '/'
   // publicPath: process.env.NODE_ENV === 'production' ? './parabah' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/trello-clone/' : '/'
 }
